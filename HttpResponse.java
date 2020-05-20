472e9993b74082b59df62a43951f5423252b7708
christianacuna <45090069+christianacuna@users.noreply.github.com>
2020-05-20 05:23:36 -0600

    Update HttpResponse
    fix convension variables

--- HttpResponse.java
+++ HttpResponse.java
@@ -6,8 +6,8 @@ import java.io.OutputStream;
 
 interface HttpResponse {
     void setVersion(String version);
-    void setStatusCode(int StatusCode);
-    void setStatusMessage(String StatusMessage);
+    void setStatusCode(int statusCode);
+    void setStatusMessage(String statusMessage);
     void appendHeader(String llave, String valor);
     OutputStream getData();
 }
